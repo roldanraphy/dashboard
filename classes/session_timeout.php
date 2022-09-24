d0f1c827465a78911541a3810a94b233ba40c1f6
roldanraphy <111292501+roldanraphy@users.noreply.github.com>
2022-09-24 14:28:30 +0800

    Delete classes directory

--- classes/session_timeout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$minutesBeforeSessionExpire=1;
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > ($minutesBeforeSessionExpire*60))) {
-  session_unset();     // remove all session variables   
-  session_destroy();   // destroy session data  
-  redirect('index.php');
-}
-$_SESSION['LAST_ACTIVITY'] = time();
-?>
